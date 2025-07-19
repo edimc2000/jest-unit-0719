c9e7a22f9cf300418e7ae1a54487e4eb8ee3a1d0
Eddie C. <edimc2000@gmail.com>
2025-07-19 00:45:59 -0500

    organized files

---
+++ units/tests/noSpace.test.js
@@ -0,0 +1,11 @@
+const noSpace = require('../units/noSpace');
+
+test('no spaces', () => {
+  expect(noSpace('')).toBe('')
+
+  expect(noSpace("Javascript")).toBe('Javascript')
+  expect(noSpace("    Hello   ")).toBe('Hello')
+  expect(noSpace(" Hello World  ")).toBe('HelloWorld')
+  expect(noSpace("Maple Q A")).toBe('MapleQA')
+
+});
---
+++ units/units/noSpace.js
@@ -0,0 +1,2 @@
+const noSpace = string => string.trim().split(' ').join('')
+module.exports = noSpace;
