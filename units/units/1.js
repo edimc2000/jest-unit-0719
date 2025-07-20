ef0edab364e3cd3c8318bd6b5e25375659d040d7
Eddie C. <edimc2000@gmail.com>
2025-07-19 22:38:28 -0500

    improvements

---
+++ units/units/1.js
@@ -0,0 +1,9 @@
+
+//Task 6: Create 2 variables. Then output the sum, product, difference, quotient 
+let number1 = 24, number2 = 10;
+//test data: 24, 10
+let add = number1 + number2;
+let multiply = number1 * number2;
+let subtract = number1 - number2;
+let divide = number1 / number2;
+let remainder = number1 % number2;
--- units/units/homework7.js
+++ units/units/homework7.js
@@ -1,4 +1,5 @@
 const hasUpperCase = string => /[A-Z]/.test(string)
+
 const noDigit = string => string.trim().split('').filter(char => /[^0-9]/.test(char)).join('')
 const noVowel = string => string.trim().split('').filter(char => /[^aeiou]/i.test(char)).join('')
 const no13 = arr => arr.map(element => element === 13 ? 0 : element)
