6a6588d0e7f0f4202d340f2f7f27c854c3a7e713
Eddie C. <edimc2000@gmail.com>
2025-07-20 23:10:10 -0500

    jean task 1

--- units/tests/jean-homework3.test.js
+++ units/tests/jean-homework3.test.js
@@ -11,6 +11,7 @@ test('Task 1: no space', () => {
 
   for (const [index, value] of givens.entries()) { 
     console.log(`Index: ${index}, Value: ${value}`)
-    //expect(randomNumAverageCalculation(givens)).toBe(expected[expected])
+   // expect(randomNumAverageCalculation(givens[index][0], givens[index][1], givens[index][2]) >= 50).toBe(expected[index])
+    expect(randomNumAverageCalculation(...value) >= 50).toBe(expected[index])
   }
 })
--- units/units/1.js
+++ units/units/1.js
@@ -1,9 +1,19 @@
+console.log("\nTask-10\n");
 
-//Task 6: Create 2 variables. Then output the sum, product, difference, quotient 
-let number1 = 24, number2 = 10;
-//test data: 24, 10
-let add = number1 + number2;
-let multiply = number1 * number2;
-let subtract = number1 - number2;
-let divide = number1 / number2;
-let remainder = number1 % number2;
+const swapFirstLastWord = (str) => {
+  const words = str.trim().split(" ").filter(w => w !== "");
+
+  if (words.length < 2) return "";
+
+  // Swap first and last
+  const first = words[0];
+  const last = words[words.length - 1];
+
+  words[0] = last;
+  words[words.length - 1] = first;
+
+  return words.join(" ");
+}
+
+testWords = ["Hello World", "I like JavaScript", "foo bar foo bar", "", "Hello", "Hello "];
+console.log(swapFirstLastWord(testWords[0]))
