d59a2d4d764538cb7436c9441d2ee7afc18d5d36
Eddie C. <edimc2000@gmail.com>
2025-07-21 17:02:36 -0500

    rev report

--- post_process/prepReport.js
+++ post_process/prepReport.js
@@ -5,7 +5,17 @@ function prepReport() {
   let html = fs.readFileSync(path, 'utf8');
 
   const customStyle = `
-    <script src="../post_process/title.js"></script>
+      <script>
+        const reStyle = () => {
+        let targetTitle = document.getElementsByTagName("i")[0]
+        targetTitle.outerHTML = "TEST RESULTS"
+        }
+
+        setTimeout(() => {
+            console.log('Delayed message after 1 second');
+            reStyle()
+        }, 100); // 1000 milliseconds = 1 second
+      </script>
 
 
 
--- units/tests/jean-homework3.test.js
+++ units/tests/jean-homework3.test.js
@@ -15,3 +15,16 @@ test('Task 1: no space', () => {
     expect(randomNumAverageCalculation(...value) >= 50).toBe(expected[index])
   }
 })
+
+
+
+test('Task 2: no space', () => {
+  givens = [[50, 50, 50]]
+  expected = [true]
+
+  for (const [index, value] of givens.entries()) { 
+    console.log(`Index: ${index}, Value: ${value}`)
+   // expect(randomNumAverageCalculation(givens[index][0], givens[index][1], givens[index][2]) >= 50).toBe(expected[index])
+    expect(randomNumAverageCalculation(...value) >= 50).toBe(expected[index])
+  }
+})
